aa660bf4508fc179dcf412c16e500bd49e02243f
tranbarsjuice <tranbarsjuice@gmail.com>
2019-07-03 13:07:27 +0200

    car sprites are now cool

--- myapp/code.py
+++ myapp/code.py
@@ -1,3 +1,386 @@
+# ID:767746162426933600
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:767746162426933600
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:737611919380832000
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:737611919380832000
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:868927350204019800
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:868927350204019800
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:65791525049015090
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:65791525049015090
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:65791525049015090
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:496192957103229000
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:496192957103229000
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:212506153503809300
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:212506153503809300
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:212506153503809300
+car.drive_forward()
+car.turn_left()
+car.turn_left()
+#####
+# ID:212506153503809300
+car.drive_forward()
+car.turn_left()
+car.turn_left()
+#####
+# ID:44863538646311980
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:44863538646311980
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:669500034554358100
+car.drive_forward()
+car.turn_left()
+#####
+# ID:669500034554358100
+car.drive_forward()
+car.turn_left()
+#####
+# ID:822181532417771100
+car.drive_forward()
+car.turn_left()
+#####
+# ID:453933492932668740
+car.drive_forward()
+car.turn_left()
+#####
+# ID:121972355052454290
+car.drive_forward()
+car.turn_left()
+#####
+# ID:145147960409918940
+car.drive_forward()
+car.turn_left()
+#####
+# ID:145147960409918940
+car.drive_forward()
+car.turn_left()
+#####
+# ID:475369466522906400
+car.drive_forward()
+car.turn_left()
+#####
+# ID:456162888148159550
+car.drive_forward()
+car.turn_left()
+#####
+# ID:456162888148159550
+car.drive_forward()
+car.turn_left()
+#####
+# ID:318758812982050300
+car.drive_forward()
+car.turn_left()
+#####
+# ID:952855080342674800
+car.drive_forward()
+car.turn_left()
+#####
+# ID:952855080342674800
+car.drive_forward()
+#####
+# ID:281548406660355300
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:281548406660355300
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:281548406660355300
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:88380233194095260
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:67195303109879420
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:67195303109879420
+car.drive_forward()
+car.turn_left()
+#####
+# ID:67195303109879420
+car.drive_forward()
+#####
+# ID:644205787970114000
+car.drive_forward()
+car.turn_left()
+#####
+# ID:644205787970114000
+car.drive_forward()
+#####
+# ID:543541709851056800
+car.drive_forward()
+#####
+# ID:42490714699782500
+car.turn_right()
+#####
+# ID:42490714699782500
+car.drive_forward()
+car.turn_left()
+car.turn_right()
+#####
+# ID:42490714699782500
+car.drive_forward()
+car.turn_left()
+#####
+# ID:452918862593256200
+car.drive_forward()
+car.turn_left()
+#####
+# ID:452918862593256200
+car.drive_forward()
+car.turn_left()
+#####
+# ID:234294829130959550
+car.drive_forward()
+car.turn_left()
+#####
+# ID:234294829130959550
+car.drive_forward()
+car.turn_left()
+#####
+# ID:234294829130959550
+car.drive_forward()
+car.turn_left()
+#####
+# ID:522718169976552800
+car.drive_forward()
+car.turn_left()
+#####
+# ID:522718169976552800
+car.drive_forward()
+car.turn_left()
+#####
+# ID:961752182684037100
+car.drive_forward()
+car.turn_left()
+car.drive_forward()
+#####
+# ID:961752182684037100
+car.drive_forward()
+car.turn_left()
+#####
+# ID:905303798460873600
+car.turn_left()
+#####
+# ID:997535774704676000
+car.turn_left()
+#####
+# ID:997535774704676000
+car.turn_left()
+#####
+# ID:964075900810016400
+car.turn_left()
+#####
+# ID:964075900810016400
+car.turn_left()
+#####
+# ID:396289516291280960
+car.turn_left()
+#####
+# ID:396289516291280960
+car.turn_left()
+#####
+# ID:566315204200456300
+car.drive_forward()
+#####
+# ID:633275624358256000
+car.drive_forward()
+car.turn_left()
+#####
+# ID:633275624358256000
+car.drive_forward()
+#####
+# ID:383419739397018800
+car.turn_left()
+#####
+# ID:383419739397018800
+car.turn_left()
+#####
+# ID:517545365085699140
+car.turn_left()
+#####
+# ID:153275714428562460
+car.turn_left()
+#####
+# ID:153275714428562460
+car.turn_left()
+#####
+# ID:97477649651924850
+car.turn_left()
+#####
+# ID:886749553679215000
+car.turn_left()
+#####
+# ID:958181367971929100
+car.drive_forward()
+car.turn_left()
+#####
+# ID:67417402641497096
+car.drive_forward()
+car.turn_left()
+#####
+# ID:231382463823116000
+car.drive_forward()
+car.turn_left()
+#####
+# ID:384147911812026000
+car.drive_forward()
+#####
+# ID:460318553786354900
+car.drive_forward()
+#####
+# ID:460318553786354900
+car.drive_forward()
+#####
+# ID:148958389611944300
+car.drive_forward()
+#####
+# ID:152898655596239260
+car.drive_forward()
+#####
+# ID:4963381641046261
+car.drive_forward()
+#####
+# ID:28932215321800790
+car.drive_forward()
+#####
+# ID:28932215321800790
+#####
+# ID:398114396095790000
+car.drive_forward()
+#####
+# ID:340090441493256200
+car.drive_forward()
+#####
+# ID:623433581518556000
+car.drive_forward()
+#####
+# ID:953738228747647000
+car.drive_forward()
+#####
+# ID:582494330268533800
+car.drive_forward()
+#####
+# ID:922536837450646000
+car.drive_forward()
+#####
+# ID:289114957380049860
+car.drive_forward()
+#####
+# ID:965266116934401500
+car.drive_forward()
+#####
+# ID:782625892997019500
+car.drive_forward()
+#####
+# ID:782625892997019500
+car.drive_forward()
+#####
+# ID:929935291991937900
+car.turn_left()
+#####
+# ID:199543766086442660
+car.turn_left()
+#####
+# ID:199543766086442660
+car.turn_left()
+#####
+# ID:985607182647956600
+car.drive_forward()
+#####
+# ID:185550686938813600
+car.drive_forward()
+#####
+# ID:185550686938813600
+car.drive_forward()
+#####
+# ID:893408020809019900
+car.drive_forward()
+#####
+# ID:893408020809019900
+car.drive_forward()
+#####
+# ID:674856077429009400
+car.drive_forward()
+#####
+# ID:93651280242077810
+car.drive_forward()
+#####
+# ID:440801733510041150
+car.drive_forward()
+#####
+# ID:495805791787786400
+car.drive_forward()
+#####
+# ID:789177287684303100
+car.drive_forward()
+#####
 # ID:440016011599518800
 car.turn_left()
 #####
