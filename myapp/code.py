6fa38fbcbba7c8b90b7d7e0af38d290cd9cbc392
Emma <emmaj7@kth.se>
2019-07-08 09:37:06 +0200

    ändrat utseende på leve 1 och 2

---
+++ myapp/code.py
@@ -0,0 +1,129 @@
+# ID:739098758938074800
+car.drive_forward()
+car.drive_forward()
+
+car.drive_backwards()
+#####
+# ID:739098758938074800
+car.drive_forward()
+car.drive_forward()
+
+car.drive_backwards()
+#####
+# ID:739098758938074800
+car.drive_forward()
+car.drive_forward()
+
+car.drive_backwards()
+#####
+# ID:487756056104564000
+car.drive_forward()
+#####
+# ID:487756056104564000
+#####
+# ID:905503420045322500
+car.turn_left()
+car.drive_forward()
+car.turn_right()
+car.drive_forward()
+#####
+# ID:905503420045322500
+car.turn_left()
+car.drive_forward()
+car.drive_forward()
+car.turn_right()
+car.drive_forward()
+#####
+# ID:905503420045322500
+car.drive_forward()
+car.drive_forward()
+car.turn_left()
+#####
+# ID:905503420045322500
+car.turn_left()
+car.drive_forward()
+car.turn_right()
+car.drive_forward()
+#####
+# ID:905503420045322500
+car.turn_left()
+car.drive_forward()
+car.drive_forward()
+car.turn_right()
+car.drive_forward()
+#####
+# ID:905503420045322500
+car.drive_forward()
+car.drive_forward()
+car.turn_left()
+#####
+# ID:317105401770710200
+car.drive_forward()
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:317105401770710200
+car.drive_forward()
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:40155467739375730
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:40155467739375730
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:410049275798847550
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:410049275798847550
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:410049275798847550
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:490825443364621300
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:490825443364621300
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:130341154803293440
+car.drive_forward()
+car.drive_forward()
+car.drive_forward()
+#####
+# ID:674676993958635500
+car.drive_forward()
+#####
+# ID:674676993958635500
+car.drive_forward()
+#####
+# ID:292125614062123200
+car.drive_forward()
+#####
+# ID:292125614062123200
+car.drive_forward()
+#####
+# ID:41782629078033760
+car.drive_forward()
+#####
+# ID:41782629078033760
+car.drive_forward()
+#####
+# ID:83958257334815330
+car.drive_forward()
+#####
+# ID:83958257334815330
+car.drive_forward()
+#####
+# ID:307567847953444800
+car.drive_forward()
+#####
