5d04a5d8df05d8b7c51d61355ba066a1c2cdf1f0
Emma <emmaj7@kth.se>
2019-07-09 13:35:26 +0200

    rensade uppfrontend-kod

--- myapp/code.py
+++ myapp/code.py
@@ -1,3 +1,63 @@
+# ID:53592401764406824
+rover.drive_forward()
+rover.drive_backwards()
+#####
+# ID:53592401764406824
+rover.drive_forward()
+rover.drive_backwards()
+#####
+# ID:281941141837596450
+rover.turn_left()
+#####
+# ID:281941141837596450
+rover.turn_left()
+#####
+# ID:26273217048531250
+rover.drive_forward()
+#####
+# ID:352110040927671000
+rover.drive_forward()
+#####
+# ID:352110040927671000
+rover.drive_forward()
+#####
+# ID:707028613131952100
+rover.drive_forward()
+#####
+# ID:707028613131952100
+rover.drive_forward()
+#####
+# ID:707028613131952100
+rover.drive_forward()
+#####
+# ID:707028613131952100
+rover.drive_forward()
+#####
+# ID:5060808608964629
+rover.turn_left()
+rover.turn_right()
+rover.turn_left()
+#####
+# ID:5060808608964629
+rover.turn_left()
+rover.turn_right()
+rover.turn_left()
+#####
+# ID:136294050604211630
+rover.turn_left()
+rover.turn_right()
+rover.turn_left()
+#####
+# ID:612116572820291500
+rover.turn_left()
+rover.turn_right()
+rover.turn_left()
+#####
+# ID:487585804172152700
+rover.turn_left()
+rover.turn_right()
+rover.turn_left()
+#####
 # ID:427459922357108160
 rover.turn_left()
 rover.turn_right()
