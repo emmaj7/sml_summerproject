d9ac0c3e4d87062059c44ce5d065eaea7b006bc0
tranbarsjuice <tranbarsjuice@gmail.com>
2019-07-09 14:53:05 +0200

    new improved collision detection

--- myapp/code.py
+++ myapp/code.py
@@ -1,3 +1,209 @@
+# ID:104715571885735550
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:104715571885735550
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:45676072313846850
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:394002660319736060
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:394002660319736060
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:394002660319736060
+rover.drive_forward()
+#####
+# ID:819426857744242600
+rover.drive_forward()
+#####
+# ID:145706049691230800
+rover.drive_forward()
+#####
+# ID:891479133254416100
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:891479133254416100
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:150618772797978530
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:125268144843635580
+rover.drive_forward()
+#####
+# ID:125268144843635580
+rover.turn_left()
+rover.drive_forward()
+rover.turn_right()
+rover.drive_forward()
+#####
+# ID:125268144843635580
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:125268144843635580
+rover.drive_forward()
+#####
+# ID:845527098900196900
+rover.drive_forward()
+#####
+# ID:845527098900196900
+rover.drive_forward()
+#####
+# ID:547510036457993000
+rover.drive_forward()
+#####
+# ID:174727770977328400
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.turn_right()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:604406515891300700
+rover.drive_forward()
+#####
+# ID:934554092863380500
+rover.drive_forward()
+#####
+# ID:426732862310116300
+rover.drive_forward()
+#####
+# ID:558837549100824260
+rover.drive_forward()
+#####
+# ID:207728404695427300
+rover.drive_forward()
+#####
+# ID:451479087063366800
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:243140721535064700
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:986715150076140300
+rover.drive_forward()
+#####
+# ID:336408353613687040
+rover.drive_forward()
+#####
+# ID:574615732915202500
+rover.drive_forward()
+#####
+# ID:574615732915202500
+rover.drive_forward()
+#####
+# ID:334565380097934660
+rover.drive_forward()
+#####
+# ID:334565380097934660
+rover.drive_forward()
+#####
+# ID:599747504307070100
+rover.drive_forward()
+#####
+# ID:484764031267896100
+rover.drive_forward()
+#####
+# ID:563313305913564400
+rover.drive_forward()
+#####
+# ID:934273236948503600
+rover.drive_forward()
+#####
+# ID:598635261779876500
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
 # ID:26273182039192156
 rover.turn_left()
 #####
