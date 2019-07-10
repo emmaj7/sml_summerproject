92cffc477e04ab91d82bedf71f5b9ccacb18ef1f
tranbarsjuice <tranbarsjuice@gmail.com>
2019-07-10 14:39:17 +0200

    .

--- svea_starter/src/svea/src/scripts/real/lidar_test.py
+++ svea_starter/src/svea/src/scripts/real/lidar_test.py
@@ -26,7 +26,7 @@ def main():
     # lidar.start()
     # lidar.start_motor()
     while not rospy.is_shutdown():
-        print(RPlidar.get_raw_scan())
+        print(lidar.get_raw_scan())
         r.sleep()
 
 if __name__ == '__main__':
