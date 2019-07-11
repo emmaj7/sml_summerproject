23f3475942e77f964d6846535c8df5d0a40bd0d5
Emma <emmaj7@kth.se>
2019-07-11 10:18:55 +0200

    help-page

--- myapp/code.py
+++ myapp/code.py
@@ -1,3 +1,364 @@
+# ID:733604771735118200
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_right()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:733604771735118200
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_right()
+rover.drive_forward()
+#####
+# ID:733604771735118200
+rover.turn_left()
+rover.drive_forward()
+rover.turn_right()
+#####
+# ID:812475645569321300
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:534512585848768400
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:305945486378099260
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:60321387852070300
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:372863870156049400
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:372863870156049400
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:851581436217058200
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:120389329684215790
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:120389329684215790
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:250398165989203500
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:250398165989203500
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:787201825420701000
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:339769595686103900
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:492172817732014000
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:492172817732014000
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:761343655302507600
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:233563940837332350
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:694601264252443000
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:685386397969163600
+for count in range(5):
+  rover.drive_forward()
+#####
+# ID:685386397969163600
+for count in range(4):
+  rover.drive_forward()
+#####
+# ID:685386397969163600
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:685386397969163600
+for count in range(5):
+  rover.drive_forward()
+#####
+# ID:685386397969163600
+for count in range(4):
+  rover.drive_forward()
+#####
+# ID:685386397969163600
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:221780347253081820
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_right()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:221780347253081820
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_right()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:146156307419594180
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:146156307419594180
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:146156307419594180
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:29456328039620172
+rover.drive_forward()
+#####
+# ID:29456328039620172
+rover.drive_forward()
+#####
+# ID:857504178213991300
+rover.drive_forward()
+rover.turn_left()
+#####
+# ID:857504178213991300
+rover.drive_forward()
+rover.turn_left()
+#####
+# ID:773113087686425200
+rover.drive_forward()
+rover.turn_left()
+#####
+# ID:558193107334291650
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:558193107334291650
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:103500657751630300
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:564995986904065500
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:564995986904065500
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:986140813295659000
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:328519786996198000
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:773809781747610800
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:773809781747610800
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:525988674335803650
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:525988674335803650
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:525988674335803650
+rover.drive_forward()
+rover.drive_forward()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:525988674335803650
+rover.drive_forward()
+rover.turn_left()
+#####
 # ID:53592401764406824
 rover.drive_forward()
 rover.drive_backwards()
