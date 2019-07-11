31eff496e540c5a7f4fe04d6752bb9cda2aaa85f
Emma <emmaj7@kth.se>
2019-07-11 11:13:58 +0200

    -

---
+++ myapp/code.py
@@ -0,0 +1,12 @@
+# ID:874290714434019500
+for count in range(5):
+  rover.drive_forward()
+#####
+# ID:559783722359033100
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:559783722359033100
+rover.drive_forward()
+rover.drive_forward()
+#####
