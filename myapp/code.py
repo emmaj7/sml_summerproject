a82b97e135315b1bb4f495037bafec4037bad21d
tranbarsjuice <tranbarsjuice@gmail.com>
2019-07-17 17:18:16 +0200

    improved wall collision algorithm

---
+++ myapp/code.py
@@ -0,0 +1,116 @@
+# ID:868948996651156500
+rover.turn_right()
+#####
+# ID:305992048325502340
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:99292777151829380
+#####
+# ID:435842813587468200
+#####
+# ID:435842813587468200
+#####
+# ID:206302653373879200
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:206302653373879200
+rover.turn_left()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:206302653373879200
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:206302653373879200
+rover.drive_backwards()
+#####
+# ID:206302653373879200
+rover.turn_left()
+rover.turn_left()
+rover.drive_forward()
+#####
+# ID:206302653373879200
+rover.turn_right()
+#####
+# ID:240336886458252800
+rover.turn_right()
+#####
+# ID:962823988522014200
+rover.turn_right()
+#####
+# ID:240122298103836350
+rover.turn_right()
+#####
+# ID:285218595006914370
+rover.turn_right()
+#####
+# ID:626802097700589000
+rover.drive_forward()
+#####
+# ID:626802097700589000
+rover.drive_forward()
+#####
+# ID:869406876119320800
+rover.turn_right()
+#####
+# ID:90579290089694340
+rover.drive_forward()
+#####
+# ID:213776957252086700
+rover.turn_right()
+#####
+# ID:43222059011783000
+rover.turn_right()
+#####
+# ID:544744120018415700
+rover.drive_backwards()
+#####
+# ID:544744120018415700
+rover.turn_right()
+#####
+# ID:544744120018415700
+rover.turn_left()
+#####
+# ID:544744120018415700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:544744120018415700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
+# ID:544744120018415700
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+rover.drive_forward()
+#####
